26377602c6a345db331bebb342756d6bf51cdef2
Omar Mendoza <59121837+mendoza000@users.noreply.github.com>
2021-04-22 13:42:01 -0400

    add firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+		apiKey: "AIzaSyBRARF3ChsOwyEaEOqYO0groxEoWPB1Hq4",
+		authDomain: "fire-crud-b0a7c.firebaseapp.com",
+		projectId: "fire-crud-b0a7c",
+		storageBucket: "fire-crud-b0a7c.appspot.com",
+		messagingSenderId: "901412398774",
+		appId: "1:901412398774:web:dda82be35ea52a33aca817",
+		measurementId: "G-YBQ2ZT8KG2"
+		};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
